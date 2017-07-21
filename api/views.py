7fab195f2c9bb6a5b02e6447d860fe34c40ef1d3
sumitrathore1313 <sumitrathore1313@gmail.com>
2017-07-21 06:23:45 +0000

    save

--- api/views.py
+++ api/views.py
@@ -10,7 +10,8 @@ from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.response import Response
 import subprocess
 import json
-
+import os 
+import pwd
 class CreateView(generics.ListCreateAPIView):
     """This class defines the create behavior of our rest api."""
     queryset = Bucketlist.objects.all()
@@ -32,15 +33,20 @@ class FileUploadView(views.APIView):
 
     def post(self, request,filename, format=None):
         my_file = request.FILES['file']
-
-        with open(filename, 'wb+') as temp_file:
+	filenam = "/home/sumitrathore1313/apps/django/django_projects/Project/ResumeParser/ResumeTransducer/data/"+filename
+        #return Response({'data': pwd.getpwuid( os.getuid() )[ 0 ]})
+	with open(filenam, 'wb+') as temp_file:
             for chunk in my_file.chunks():
                 temp_file.write(chunk)
-        input_file = filename
-        output_file = "sample"
-        cmd = "java -cp 'ResumeParser/ResumeTransducer/bin/*:ResumeParser/GATEFiles/lib/*:ResumeParser/GATEFiles/bin/gate.jar:ResumeParser/ResumeTransducer/lib/*' code4goal.antony.resumeparser.ResumeParserProgram %s %s.json" % (input_file, output_file)
-        subprocess.Popen(cmd, shell=True)
-        with open('sample.json') as json_data:
+        #return Response({'data': "hello"})
+	input_file = filename
+        output_file = filename.split('.')[0]
+        cmd = "java -cp 'bin/*:../GATEFiles/lib/*:../GATEFiles/bin/gate.jar:lib/*' code4goal.antony.resumeparser.ResumeParserProgram data/%s data/%s.json" % (input_file, output_file)
+	#cmd = "java -cp 'ResumeParser/ResumeTransducer/bin/*:ResumeParser/GATEFiles/lib/*:ResumeParser/GATEFiles/bin/gate.jar:ResumeParser/ResumeTransducer/lib/*' code4goal.antony.resumeparser.ResumeParserProgram %s %s.json" % (input_file, output_file)
+        os.chdir("/home/sumitrathore1313/apps/django/django_projects/Project/ResumeParser/ResumeTransducer/")
+	out = subprocess.check_output(cmd, shell=True)
+        #return Response({'data': out})
+	with open("/home/sumitrathore1313/apps/django/django_projects/Project/ResumeParser/ResumeTransducer/data/%s.json" % (output_file)) as json_data:
             data = json.load(json_data)
     
         return Response({'data': data})
